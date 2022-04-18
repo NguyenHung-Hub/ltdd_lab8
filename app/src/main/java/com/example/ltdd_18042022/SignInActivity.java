2be00425cf7040bd59813b912eb78941bb6a9dd5
NguyenHung-Hub <daminhnguyenhung@gmail.com>
2022-04-18 17:15:50 +0700

    screen 1, 2, 3

---
+++ app/src/main/java/com/example/ltdd_18042022/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.ltdd_18042022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
